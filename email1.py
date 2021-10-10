fdf93955a225feccbe4fb3e910ce1657604a04de
Amzad hossain <amzad.rafi@northsouth.edu>
2021-10-10 19:06:58 +0600

    email done sms done but

---
+++ email1.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import csv 
+import requests
+
+import smtplib
+from voice_assi import Assistant
+from twilio.rest import Client
+
+class MailandCont():
+    def __init__(self,name):
+        self.name = name
+        self.VA= Assistant()
+        self.setting= self.VA.settings
+        self.email=self.setting.user_email
+        self.user_password=self.setting.user_pass
+        self.sid=self.setting.Sid
+        self.ath=self.setting.ath
+        self.Tnumber=self.setting.Tnumber
+        
+        
+        
+    
+    def find(self):
+        self.filename="info.csv"
+        with open(self.filename,'r',encoding="utf8") as f:
+            reader = csv.reader(f)
+
+
+            for row in reader:
+
+                if row[1].lower()== self.name.lower():
+                
+                    user_name = row[1]
+                    user_email = row[0]
+                    user_phone = row[3]
+                    #print(f"user name {user_name} user email {user_email} user_phone{user_phone}")
+
+
+                    return {"user_name": str(user_name).lower(), 
+                        "user_email": str(user_email).lower(),
+                        "user_phone": f"+880{user_phone}",
+                        }
+
+
+
+
+#this fucntion take name as arg 
+    def sendmail(self):
+
+
+        reciver=self.find()
+        m=reciver["user_email"]
+        server=smtplib.SMTP_SSL("smtp.gmail.com",465)
+        server.starttls()
+        server.login(self.email,self.user_password)
+        server.sendmail(self.email,str(m),"this is a spam msg don't reply")
+        print(" mail is send to ")
+        server.quit()
+
+
+
+    def sendsms(self):
+        reciver=self.find()
+        test_msg="this is a test masg "
+        m=reciver["user_phone"]
+        client=Client(self.sid,self.ath)
+        message=client.messages.create(to="+880 1728118376",from_=self.Tnumber,body=test_msg)
+        print(message.sid)
+        
+if __name__ == '__main__':
+    test=MailandCont("Mobasshir")
+    e=test.find()
+    print(e["user_phone"])
+    test.sendsms()
--- settings.py
+++ settings.py
@@ -6,4 +6,12 @@ class Settings():
         self.address="Bangladesh"
         self.voice_type= 1
         self.voice_rate= -70
+        #email
+        self.user_email="azhossain144@gmail.com"
+        self.user_pass="test@mail124"
+        #sms
+        self.Sid = "AC801efb59d27cd5186d1f0a64f2c53d2e"
+        self.ath="ab3cd9c428ffe0e4509603038d51532b"
+        self.Tnumber="+18128182070"
+        
 
